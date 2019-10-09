732bb2654f8165fac5af65da066e27639b972b2c
Orosz <oros.norbert1@gmail.com>
2019-10-09 22:21:42 +0200

    Maven build modifications

--- src/main/java/com/sample/webapp/controllers/Controller.java
+++ src/main/java/com/sample/webapp/controllers/Controller.java
@@ -1,6 +1,5 @@
 package com.sample.webapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
